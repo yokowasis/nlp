cda1448b1afc3e7b53305160e78c44559260ccb3
yokowasis@host2 <yokowasis@gmail.com>
2024-10-15 04:08:02 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use the official Python 3.10.14 image from the Docker Hub
+FROM python:3.10.14-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file into the container at /app
+COPY requirements.txt /app/
+
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of your application files (optional)
+# COPY . /app
+
+# Command to run your application (update this according to your app)
+CMD ["python", "backend.py"]
---
+++ backend.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from fn import encode, summarize, translate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+# Sample data
+data = {
+    "hello": "world",
+    "man": [
+        "aaa", "bbb", "ccc"
+    ]
+}
+
+# POST request handler
+
+
+@app.route("/api/vectorize", methods=['POST'])
+def api_vectorize():
+    inputs = request.get_json()
+    vec = encode(inputs['text'])
+    return jsonify(vec)
+
+
+@app.route("/api/summarize", methods=['POST'])
+def api_summarize():
+    inputs = request.get_json()
+    summary = summarize(inputs['text'])
+    return jsonify(summary)
+
+
+@app.route("/api/translate", methods=['POST'])
+def api_translate():
+    inputs = request.get_json()
+    translation = translate(inputs['text'])
+    return jsonify(translation)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(data)
+
+
+@app.route('/', methods=['GET'])
+def get_index():
+    return """
+    <pre>
+    Usage :
+    POST /api/vectorize
+    {
+        "text": "hello world"
+    }
+    
+    POST /api/summarize
+    {
+        "text": "hello world"
+    }
+
+    POST /api/translate
+    {
+        "text": "Halo Dunia"
+    }
+
+    </pre>
+"""
+
+
+debugenv = os.getenv('DEBUG')
+if debugenv == "True":
+    app.run(debug=True, port=int(os.getenv('PORT') or "3012"))
+else:
+    app.run(debug=False, port=int(os.getenv('PORT') or "3012"))
