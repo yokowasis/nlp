ab6e04fe3f5d54d42380859d9a65a4010780e08d
yokowasis@host2 <yokowasis@gmail.com>
2024-10-18 16:44:48 +0000

    fastapi

--- backend.py
+++ backend.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse, HTMLResponse
+from pydantic import BaseModel, WithJsonSchema
 from fn import encode, summarize, translate
-import psycopg2
 from dotenv import load_dotenv
+from typing import List, Annotated
+import psycopg2
 import os
+import json
 
 load_dotenv()
 
@@ -23,24 +27,25 @@ cursor = conn.cursor()
 
 def query(sql):
     try:
-
         # Execute a query
         cursor.execute(sql)
         records = cursor.fetchall()
         return records
-
     except Exception as e:
         print(f"An error occurred: {e}")
-    # finally:
-    #     # Close the cursor and connection
-    #     if cursor:
-    #         cursor.close()
-    #     if conn:
-    #         conn.close()
+        raise HTTPException(status_code=500, detail="Database query error")
 
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Sample data
 data = {
@@ -50,82 +55,80 @@ data = {
     ]
 }
 
-# POST request handler
+
+class VectorizeBody(BaseModel):
+    text: str
 
 
-@app.route("/api/vectorize", methods=['POST'])
-def api_vectorize():
-    inputs = request.get_json()
-    vec = encode(inputs['text'])
-    return jsonify(vec)
+@app.post(
+    "/api/vectorize",
+    description="This endpoint takes a text as input and returns a vectorized representation of it. The Vector size is 1024.", name="Text to Vector",
+    response_model=List[float],
+
+)
+async def api_vectorize(inputs: VectorizeBody = Body(..., example={"text": "Hello, world!"})):
+    vec = encode(inputs.text)
+    return JSONResponse(content=json.loads(vec))
 
 
-@app.route("/api/summarize", methods=['POST'])
-def api_summarize():
-    inputs = request.get_json()
-    summary = summarize(inputs['text'])
-    return jsonify(summary)
+class SummarizeBody(BaseModel):
+    text: str
 
 
-@app.route("/api/translate", methods=['POST'])
-def api_translate():
-    inputs = request.get_json()
-    lang = inputs['lang']
-    translation = translate(inputs['text'], lang=lang)
-    return jsonify(translation)
+@app.post("/api/summarize", name="Summarize Text", description="This endpoint takes a text as input and returns a summary of it.")
+async def api_summarize(inputs: SummarizeBody):
+    summary = summarize(inputs.text)
+    return JSONResponse(content=summary)
 
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify(data)
+class TranslateBody(BaseModel):
+    text: str
+    target_language: str
 
 
-@app.route("/api/semantic-search", methods=['POST'])
-def api_semantic_search():
-    inputs = request.get_json()
-    text = inputs['text']
+@app.post("/api/translate", name="Translate Text", description="This endpoint takes a text as input and returns a translated text in the specified language.")
+async def api_translate(inputs: TranslateBody):
+    lang = inputs.target_language
+    translation = translate(inputs.text, lang=lang)
+    return JSONResponse(content=translation)
+
+
+class SemanticBody(BaseModel):
+    text: str
+    table: str
+    retrieved_columns: str
+    target_column: str
+    limit: int
+
+
+@app.post("/api/semantic-search", name="Semantic Search", description="This endpoint takes a text as input and returns a list of rows from a table that match the text. The retrieved columns are the columns that you want to retrieve from the table separated by comma. The target column is the column in the database that has the type of `vector`.")
+async def api_semantic_search(inputs: SemanticBody):
+    text = inputs.text
     embedding = encode(text)
-    table = inputs['table']
-    select_column = inputs['select_column']
-    target_column = inputs['target_column']
-    limit = inputs['limit']
+    table = inputs.table
+    select_column = inputs.retrieved_columns
+    target_column = inputs.target_column
+    limit = inputs.limit
 
     sql = f"SELECT {select_column} FROM {table} ORDER BY {target_column} <#> '{embedding}' LIMIT {limit}"
 
     rows = query(sql)
 
-    return jsonify(rows)
+    return JSONResponse(content=rows)
 
 
-@app.route('/', methods=['GET'])
-def get_index():
-    return """
-    <pre>
-    Usage :
-    POST /api/vectorize
-    {
-        "text": "hello world"
-    }
-    
-    POST /api/summarize
-    {
-        "text": "hello world"
-    }
-
-    POST /api/translate
-    {
-        "text": "Halo Dunia",
-        "lang" : "english"
-    }
-
-    POST /api/semantic-search
-    {
-        "text": "hello world",
-        "table" : "comments",
-        "select_column" : "id,text",
-        "target_column" : "text_vector",
-        "limit" : 10
-    }
-
-    </pre>
-"""
+@app.get("/")
+async def get_index():
+    return HTMLResponse("""
+      <html>
+      <head>
+          <title>NLP API</title>
+      </head>
+      <body>
+          <h1>NLP API</h1>
+          <p>This is a simple NLP API that uses FastAPI and PostgreSQL.</p>
+          <p>You can use it to vectorize text, summarize text, translate text, and perform semantic search.</p>
+          <p>Refer to the <a href="/docs">API documentation</a> for more information.</p>
+      </body>
+      </html>
+""")
